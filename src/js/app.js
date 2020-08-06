153275750b18e69d3e1ac8c13d102e3f5133b9cd
Alexey Lebedev <vorresti@ya.ru>
2020-08-06 16:10:54 +0300

    when choice flex item shown current values

--- src/js/app.js
+++ src/js/app.js
@@ -36,13 +36,34 @@ changeContainerButtons.forEach((button) => button.addEventListener('click', (eve
 const selectFlexItemButtons = document.querySelectorAll('.select-item-button');
 
 selectFlexItemButtons.forEach((button) => button.addEventListener('click', (event) => {
-  const currentFlexItemsButtons = event.target.closest('.items-buttons').children;
+  const flexItemsButtons = Array.from(event.target.closest('.items-buttons').children);
+  // console.log(flexItemsButtons);
 
+  const flexPropertiesButtons = Array.from(event.target.parentElement.nextElementSibling.children);
+  // console.log(flexPropertiesButtons);
+
+  const descriptionElement = event.target.closest('.container').nextElementSibling.firstElementChild;
+  // console.log(descriptionElement);
+
+  // по клику на выбор элемента выбираем сам элемент из контейнера, считываем его data-value
   const flexItems = Array.from(event.target.closest('.container').lastElementChild.children);
   const [ flexItem ] = flexItems.filter( item => item.innerText === event.target.innerText);
-  console.log(flexItem);
+  // console.log(flexItem);
 
-  drawUI.clearActiveButtons(currentFlexItemsButtons);
+  const [ flexProprtyButton ] = flexPropertiesButtons.filter( button => button.dataset.value === flexItem.dataset.value);
+  console.log(flexProprtyButton);
+
+  const flexItemValue = flexItem.dataset.value;
+  // console.log(flexItemValue);
+
+  const flexItemProperty = event.target.closest('.container').lastElementChild.dataset.prop;
+  // console.log(flexItemProperty);
+
+  const descriptionText = descriprionGenerator.getDescription(flexItemProperty, flexItemValue);
+  // console.log(descriptionText);
+
+  drawUI.changeActivePropertyButton(flexPropertiesButtons, flexProprtyButton);
+  drawUI.clearActiveButtons(flexItemsButtons);
   drawUI.setActiveButton(event.target);
 }));
 
@@ -58,12 +79,12 @@ changeFlexItemPropertyButtons.forEach((button) => button.addEventListener('click
   exampleCssField = event.target.closest('.container').nextElementSibling.lastElementChild;
   changableElementName = event.target.closest('.container').dataset.id;
 
+  // определяем itemFlexElement по признаку 'active' и устанавливаем уму data-value такой же как у кнопки
   const flexItemsButtons = Array.from(event.target.closest('.container').firstElementChild.firstElementChild.children);
   const [ activeFlexItemButton ] = flexItemsButtons.filter( element => element.classList.contains('active'));
   const activeElementIndex = +(activeFlexItemButton.innerText) - 1;
   const flexItemsElements = event.target.closest('.container').lastElementChild.children;
   itemFlexElement = flexItemsElements[activeElementIndex];
-
   itemFlexElement.setAttribute('data-value', event.target.dataset.value);
 
   drawUI.changeActivePropertyButton(propertyButtons, event.target);
